7dc41dfbbd25e4e6835c428e399d3512eaded551
holidayjo <ossam1996@gmail.com>
2023-01-16 05:25:28 +0900

    added initial version

--- Julia/mnist_tutorial.jl
+++ Julia/mnist_tutorial.jl
@@ -6,7 +6,10 @@ cars |> @select(:Acceleration, :Name) |> collect
 
 function foo(data, origin)
     df = data |> @filter(_.Origin==origin) |> DataFrame
-    return df |> @vlplot(:point, :Acceleration, :Miles_per_Gallon)
+    return df |> @vlplot(:point, :Acceleration, :Horsepower)
 end
 
 p = foo(cars, "USA")
+p |> save("foo.png")
+
+# so far, I have made very simple dataset process.
